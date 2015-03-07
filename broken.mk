2dc9606738c65739d70312cce8b2da106ad2b828
FuckingShitUp <kengreenwood420666@gmail.com>
2015-03-07 12:51:07 -0600

    Broke it

--- broken.mk
+++ broken.mk
@@ -4,12 +4,12 @@ TARGET_SCREEN_HEIGHT := 960
 
 $(call inherit-product, device/motorola/condor/full_condor.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Broken stuff.
+$(call inherit-product, vendor/broken/config/common_full_phone.mk)
 
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_condor
+PRODUCT_NAME := broken_condor
 PRODUCT_RELEASE_NAME := MOTO E
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
